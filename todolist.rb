666aeadd434a30379c4186f76283fa6ffe24594a
Eric Liu <ericliu4943@gmail.com>
2015-11-21 22:58:06 -0800

    Broke out functions into their own methods.

--- todolist.rb
+++ todolist.rb
@@ -1,21 +1,36 @@
+$todos = []
+
 def todolist
-	todos = []
+	
 	exit = false
 	until exit # until exit is true
-	puts "To Dos"
-	if todos.empty?
-		puts "You're clear!"
-	else
-		(todos.count).times {|i| puts("[" + (i+1).to_s + "] " + todos[i])}
-	end
-	puts "================="
-	puts "1. Add item"
-	puts "2. Complete item"
-	puts "3. Exit"
-	input = gets.chomp.to_i
+		puts "To Dos"
+		if $todos.empty?
+			puts "You're clear!"
+		else
+			($todos.count).times {|i| puts("[" + (i+1).to_s + "] " + $todos[i])}
+		end
+		puts "================="
+		puts "1. Add item"
+		puts "2. Complete item"
+		puts "3. Exit"
+		input = gets.chomp.to_i
 
-	if input == 1 # add item
-		added = false
+		if input == 1 # add item
+			add_item # call method to add item
+		elsif input == 2 #complete item
+			complete_item 
+		elsif input == 3 # exit
+			exit?
+		end
+		system "clear"
+	end
+	return self
+end
+
+
+def add_item # option 1
+	added = false
 		until added
 			puts "What do you gotta do?"
 			new_do = gets.chomp
@@ -23,38 +38,38 @@ def todolist
 			answer = gets.chomp.downcase
 			if answer == "y"
 				new_do = new_do.downcase.capitalize + "."
-				todos << new_do
+				$todos << new_do
 				added = true
 			elsif answer == "e"
 				added = true # exit the loop
 			end 
 		end
-	elsif input == 2 #complete item
-		completed = false
+end
+
+def complete_item
+	completed = false
 		until completed
 			puts "Which number did you finish (Number or E)?"
 			answer = gets.chomp
 			num_answer = answer.to_i
 			if answer.downcase == "e"
 				completed = true
-			elsif todos[num_answer-1] == nil
+			elsif $todos[num_answer-1] == nil
 				puts "That's not a todo!"
 			else
 				puts "Are you sure you want to complete number #{answer} (Y/N or E)?"
 				confirm = gets.chomp.downcase
 				if confirm == "y"
-					todos.delete(todos[num_answer-1])
+					$todos.delete($todos[num_answer-1])
 					completed = true
 				elsif confirm == "e"
 					completed = true
 				end
 			end
 		end
-	elsif input == 3 # exit
-		puts "Alrighty! Till next time!"
-		exit = true
-	end
-	system "clear"
 end
-return self
+
+def exit? # current bug: not being able to exit. Try global variable?
+	puts "Alrighty! Till next time!"
+	return exit = true
 end
